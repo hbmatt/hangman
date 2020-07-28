7d8e5e431a34add2a18cccad826ffc5a31b320f1
hbmatt <hannahbmatt@gmail.com>
2020-07-28 14:50:13 -0500

    Updated README.md and hangman.rb. Added save.yaml.

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-A project from The Odin Project to build a command line Hangman game using Ruby.
+A project from The Odin Project to build a command line Hangman game using Ruby. Features the ability to save and load a game in progress.
--- lib/hangman.rb
+++ lib/hangman.rb
@@ -1,29 +1,113 @@
+require 'yaml'
+
+module SaveLoad
+  def save_game(word, word_display_array, word_display, strikes, incorrect_letters)
+    saved_variables = [word, word_display_array, word_display, strikes, incorrect_letters]
+
+    data = { 'word' => word, 'word_display_array' => word_display_array, 'word_display' => word_display, 'strikes' => strikes, 'incorrect_letters' => incorrect_letters}
+
+    File.open('save.yaml', 'w') do |file|
+      file.write(data.to_yaml)
+    end
+
+    puts "\nYour game has been saved.\n"
+
+    exit_game()
+  end
+
+  def exit_game
+    puts "\nKeep playing? [Y/N]"
+
+    answer = gets.chomp.upcase
+
+    until answer == "Y" || answer == "N"
+      puts "\nYou can't do that! Keep playing? [Y/N]"
+      answer = gets.chomp.upcase
+    end
+
+    if answer == "N"
+      puts "\nGoodbye!"
+      exit
+    end
+  end
+
+  def load_file
+    if File.exist?('save.yaml')
+      YAML.load(File.read('save.yaml'))
+    else
+      puts "\n\nThere is no save information."
+      puts "\n\nStarting new game!\n\n"
+    end
+  end
+end
+
 class Game
+  include SaveLoad
+  attr_accessor :word, :word_display_array, :word_display, :strikes, :incorrect_letters
+
   def initialize
     @word = []
     @word_display_array = []
     @word_display = ''
     @strikes = 0
+    @incorrect_letters = []
     @player = Player.new
   end
 
-  def start_game
+  def play_game
     puts "\n+------------------------------------------------+
     \rLet's Play Hangman!"
 
     choose_word()
-
-    puts "\nThe secret word is #{@word.length} letters long.\n"
     make_board()
+    ask_load()
+
+    puts "\nThe secret word is #{@word.length} letters long.
+    \rTo save the game at any point, type 'save'.\n\n"
+
 
     until @strikes > 5 || @word == @word_display_array
-      display_board()
-      @player.guess_letter
-      check_letter(@player.guess)
       update_board()
+      display_board() 
+      @player.guess_letter(@incorrect_letters)
+      check_save()
+      check_letter(@player.guess)
     end
 
     end_game()
+  end
+
+  def ask_load
+    puts "\nLoad saved game? [Y/N]"
+    answer = gets.chomp.upcase
+
+    until answer == "Y" || answer == "N"
+      puts "\nYou can't do that! Load game? [Y/N]"
+      answer = gets.chomp.upcase
+    end
+
+    if answer == "Y"
+      load_game()
+    else
+      puts "\n\nStarting new game!\n\n"
+    end 
+  end
+
+  def load_game()
+    load = load_file()
+    @word = load['word']
+    @word_display_array = load['word_display_array']
+    @word_display = load['word_display']
+    @strikes = load['strikes']
+    @incorrect_letters = load['incorrect_letters']
+    
+    puts "\n\nYour game has been loaded."
+  end
+
+  def check_save()
+    if @player.guess == 'save'
+      save_game(@word, @word_display_array, @word_display, @strikes, @incorrect_letters)
+    end
   end
 
   def choose_word
@@ -33,7 +117,7 @@ class Game
       word = word.gsub("\r\n", '')
       word.length >= 5 && word.length <= 12 ? word_list.push(word) : next
     end
-    @word = word_list.sample.split('')
+    @word = word_list.sample.downcase.split('')
   end
 
   def make_board
@@ -46,19 +130,21 @@ class Game
   end
 
   def display_board
-    puts "\n#{@word_display}  ||  Strikes: #{@strikes}  ||  Missed letters: #{@player.incorrect_letters.join(' ')}\n\n"
+    puts "\n+------------------------------------------------+\n
+    \r#{@word_display}  ||  Strikes: #{@strikes}  ||  Missed letters: #{@incorrect_letters.join(' ')}\n"
   end
 
   def check_letter(guess)
     if @word.include?(guess)
       add_letter(@player.guess)
+    elsif guess == 'save'
+      return
     else
       strike_letter(@player.guess)
     end
   end
 
   def add_letter(guess)
-    @player.correct_letters.push(@player.guess)
     @word_display_array.each_index do |i|
       if @word[i] == guess
         @word_display_array[i] = guess
@@ -67,7 +153,7 @@ class Game
   end
 
   def strike_letter(guess)
-    @player.incorrect_letters.push(guess)
+    @incorrect_letters.push(guess)
     @strikes += 1
   end
 
@@ -91,7 +177,7 @@ class Game
     end
 
     if answer == "Y"
-      Game.new.start_game
+      Game.new.play_game
     else
       puts "\nGoodbye!"
       exit
@@ -100,23 +186,23 @@ class Game
 end
 
 class Player
-  attr_accessor :guess, :incorrect_letters, :correct_letters
+  include SaveLoad
+  attr_accessor :guess
 
   def initialize
     @guess = []
-    @incorrect_letters = []
-    @correct_letters = []
   end
 
-  def guess_letter
-    puts "Guess a letter:"
+  def guess_letter(incorrect_letters)
+    puts "\nGuess a letter:"
     @guess = gets.chomp.downcase
 
-    while @guess == "/[^a-z]/" || @guess.length > 1 || @incorrect_letters.include?(@guess)
+    unless @guess == 'save' || @guess == "/[a-z]/" || @guess.length == 1 || !incorrect_letters.include?(@guess)
       puts "\nYou can't do that! Guess a letter:"
       @guess = gets.chomp.downcase
     end
   end
 end
 
-Game.new.start_game
+
+Game.new.play_game
